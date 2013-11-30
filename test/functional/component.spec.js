c1bfad37bb987e6aff8899cfb9c93814a7679759
Ryan Florence <rpflorence@gmail.com>
2013-11-29 23:02:24 -0700

    got component test passing :D

--- test/functional/component.spec.js
+++ test/functional/component.spec.js
@@ -5,19 +5,26 @@ var sinon = require('sinon');
 
 describe('component', function() {
 
-  it('renders the template correctly', function() {
+  it('renders the template correctly', function(done) {
     var locals = {objectName: 'XFooComponent'};
-    var component = render('app/components/component.js.hbs', locals);
-    var template = render('app/templates/components/component.hbs.hbs', locals);
-    loom('-sq component x-foo').out.should.equal(component + template);
+    render('app/components/component.js.hbs', locals, function(component) {
+      render('app/templates/components/component.hbs.hbs', locals, function(template) {
+        loom('-sq component x-foo', function(env) {
+          env.out.should.equal(component + template);
+          done();
+        });
+      });
+    });
   });
 
-  it('requires a - in the component name', function() {
+  it('requires a - in the component name', function(done) {
     var mock = sinon.mock(msg);
     mock.expects('error').once();
-    loom('-sq component foo');
-    mock.verify();
-    mock.restore();
+    loom('-sq component foo', function(env) {
+      mock.verify();
+      mock.restore();
+      done();
+    });
   });
 
 });
